319239278acd78af5d4b21eb09ba8418c67f276f
Vonte <devonte.mote@gmail.com>
2025-03-20 13:25:18 -0700

    Add files via upload

---
+++ labexercises/exercise4.js
@@ -0,0 +1,19 @@
+console.log(5 + 1);
+
+console.log(10 -8);
+
+console.log(3 * 6);
+
+console.log(22 / 4);
+
+const newName = "Sharkeisha";
+    console.log("Hello " + newName);
+
+    function calculateTriangleArea(base, height) {
+        return 0.5 * base * height;
+    }
+
+const base = 5;
+const height = 10;
+const area = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + area);
