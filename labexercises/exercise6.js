cfe850580bf888589fc4506a0a4d08b0fa936a27
Vonte <devonte.mote@gmail.com>
2025-03-20 15:03:05 -0700

    Add files via upload

---
+++ labexercises/exercise6.js
@@ -0,0 +1,47 @@
+const book = {
+    title: 'The Fallen King',
+    description: 'A King who has fallen from his throne, now seeks to start a new life.',
+    author: 'Roscoe Bojangles',
+    pages: 350
+}
+
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.pages);
+
+book.description = 'A king who now needs to learn to live as a man.';
+console.log(book.description);
+
+const allBooks = [
+    {
+    title: 'Rising Above',
+    description: 'A journe of how to rise above the challenges of life.',
+    auhtor: 'John Doe',
+    pages: 260
+    },
+
+    {
+      title: "Heaven's Warlord",
+      description: 'A story of a warrior who fights for the heavens.',
+      author: 'Jane Buckheimer',
+      pages: 380
+    },
+    {
+        title: 'In the Depths',
+        description: 'A story of someone who has fallen deep into the depths of their mind',
+        author: 'Billy Bob',
+        pages: 437
+    },
+    {
+        title: 'The Phoenix Soldier',
+        description: 'A Naby soldier returns from the dead to fight for his country.',
+        author: 'Jill Smith',
+        pages: 475
+    },
+    {
+        title: 'What Lies Beneath',
+        description: 'A girl falls for a guy who has a dark secret.',
+        author: 'Michael Crabtree',
+        pages: 437
+    }];
