9d47956c0ebcea0d3ce31888399b9b4085dbe423
Vonte <devonte.mote@gmail.com>
2025-03-20 17:23:30 -0700

    Add files via upload

---
+++ labexercises/exercise7.js
@@ -0,0 +1,24 @@
+console.log(5 + 1); //adding 5 + 1. Result = 6
+
+console.log(10 -8); //subtractng 10 - 8. Result = 2
+
+console.log(3 * 6); //multiplying 3 * 6. Result = 18
+
+console.log(22 / 4); //dividing 22 by 4. Result = 5.5
+
+console.log(-50 - 25); //subtracting -50 from 25. Result = -75
+
+//declaring a variable called newName and assigning it the value of "Sharkeisha"
+const newName = "Sharkeisha";
+    console.log("Hello " + newName); //console.log will return "Hello Sharkeisha"
+
+//declaring a function called calculateTriangleArea that takes in two parameters, base and height
+    function calculateTriangleArea(base, height) {
+        return 0.5 * base * height;
+    }
+
+const base = 5;
+const height = 10;
+const area = calculateTriangleArea(base, height);
+console.log("The area of the triangle is: " + area);
+// the output will result in "The area of the triangle is: 25". 
