da9c556a5b44a98b9735368955809b77573ad5f0
Vonte <devonte.mote@gmail.com>
2025-05-25 23:22:39 -0700

    part 1 lab 8 commit

--- module5/calculator-appLab/index.js
+++ module5/calculator-appLab/index.js
@@ -5,6 +5,11 @@ const port = 3000; // set the port to 3000
 const testRoutes = require('./routes/myTestRoutes');
 const calculatorRoutes = require('./routes/calculatorRoutes'); // import the calculator routes
 
+// This imports swagger, creates a swagger document, and create a route specfically for the swagger API docs
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // 
+
 app.use('/',express.static('public')); // serve static files from the public directory
 app.use('/calculator', calculatorRoutes); // use the calculator routes for any requests to /calculator
 app.use('/test', testRoutes);
